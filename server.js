36ca2a6cd31cb13f130c38ca9207ccae6f61838e
Anandkr21 <anandkumargupta.akg15@gmail.com>
2023-06-05 21:32:22 +0530

    swagger testing

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express")
+const swaggerJSdoc = require("swagger-jsdoc")
+const swaggerUi = require("swagger-ui-express")
+const {UserModel} = require('./model/userModel')
+const { connection } = require("./config/db")
+const { userRouter } = require("./route/userRoute")
+require("dotenv").config()
+const app = express()
+
+app.use(express.json())
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Learning Swagger",
+            version: "1.0.0"
+        },
+        servers: [
+            {
+                url: "http://localhost:5050"
+            }
+        ]
+    },
+    apis: ["./routes/*.js"]
+}
+
+const swaggerSpec = swaggerJSdoc(options)
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+app.use("/users", userRouter)
+
+
+app.listen(process.env.port, async () => {
+    try {
+        await connection
+        console.log("Connected to the DB")
+    } catch (err) {
+        console.log(err)
+    }
+    console.log(`Running at port ${process.env.port}`)
+})
